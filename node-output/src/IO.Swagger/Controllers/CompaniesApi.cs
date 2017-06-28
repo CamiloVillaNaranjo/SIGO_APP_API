cda4315ef28c58569d5ebfa599857daab353a60b
cvillanexos <cavilla@nexos-software.com.co>
2017-06-28 17:06:10 +0000

    Generated code by SwaggerHub

--- node-output/src/IO.Swagger/Controllers/CompaniesApi.cs
+++ node-output/src/IO.Swagger/Controllers/CompaniesApi.cs
@@ -94,15 +94,15 @@ namespace IO.Swagger.Controllers
         /// </summary>
         /// <remarks>From an RFC emsior - RFC receptor relationship is queried whether the relationship is in tests or production.</remarks>
         /// <param name="country">Refrences to the country that will use the web service (meanwhile only MX is allowed to consume)</param>
-        /// <param name="fiscalNumber">References to RFC of Emitter.</param>
-        /// <param name="receiverFiscalNumber">References to the RFC of Receiver</param>
+        /// <param name="emitterNumber">References to RFC of Emitter.</param>
+        /// <param name="receiverNumber">References to the RFC of Receiver</param>
         /// <response code="200">Successful operation</response>
         /// <response code="404">Not found</response>
         [HttpGet]
         [Route("/cvillanexos/NexosSigostore/beta/companies/{country}/getRelationStatus")]
         [SwaggerOperation("GetRelationStatus")]
         [SwaggerResponse(200, type: typeof(RelationShips))]
-        public virtual IActionResult GetRelationStatus([FromRoute]string country, [FromQuery]string fiscalNumber, [FromQuery]string receiverFiscalNumber)
+        public virtual IActionResult GetRelationStatus([FromRoute]string country, [FromQuery]string emitterNumber, [FromQuery]string receiverNumber)
         { 
             string exampleJson = null;
             
