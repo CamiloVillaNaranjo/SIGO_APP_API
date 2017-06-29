ae9a38e9deab4fe0caf3af7ded39f3a997f9d3eb
cvillanexos <cavilla@nexos-software.com.co>
2017-06-29 03:34:48 +0000

    Generated code by SwaggerHub

--- node-output/src/IO.Swagger/Controllers/CompanyApi.cs
+++ node-output/src/IO.Swagger/Controllers/CompanyApi.cs
@@ -87,29 +87,5 @@ namespace IO.Swagger.Controllers
             : default(CfdiCredential);
             return new ObjectResult(example);
         }
-
-
-        /// <summary>
-        /// Verifies that the status of the current relationship between receiver and emitter.
-        /// </summary>
-        /// <remarks>From an RFC emsior - RFC receptor relationship is queried whether the relationship is in tests or production.</remarks>
-        /// <param name="country">Refrences to the country that will use the web service (meanwhile only MX is allowed to consume)</param>
-        /// <param name="numberEmitter">References to RFC of Emitter.</param>
-        /// <param name="numberReceiver">References to the RFC of Receiver</param>
-        /// <response code="200">Successful operation</response>
-        /// <response code="404">Not found</response>
-        [HttpGet]
-        [Route("/cvillanexos/NexosSigostore/beta/company/{numberEmitter}/getRelationStatus")]
-        [SwaggerOperation("GetRelationStatus")]
-        [SwaggerResponse(200, type: typeof(RelationShips))]
-        public virtual IActionResult GetRelationStatus([FromQuery]string country, [FromRoute]string numberEmitter, [FromQuery]string numberReceiver)
-        { 
-            string exampleJson = null;
-            
-            var example = exampleJson != null
-            ? JsonConvert.DeserializeObject<RelationShips>(exampleJson)
-            : default(RelationShips);
-            return new ObjectResult(example);
-        }
     }
 }
